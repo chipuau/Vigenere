1f1691271b44e50927ee36776553614af0ccf4f1
Megan Chipman <m.chipman52@gmail.com>
2018-12-03 22:06:21 -0500

    Encryption and Decryption
    Completed Decryption function and fixed encryption to produce the correct output.

--- Vigenere/Vigenere/Encrypt.cpp
+++ Vigenere/Vigenere/Encrypt.cpp
@@ -14,77 +14,113 @@ Encrypt::~Encrypt()
 
 void Encrypt::DoEncryption(std::string plainText, std::string key)
 {
-	//Convert Plaintext to all lowercase: 
-	for (int i = 0; i < plainText.length(); i++)
-	{
-		if (isupper(plainText[i]))
-		{
-			plainText[i] = tolower(plainText[i]); 
-		}
-	}
-
-	//Convert Key to all lowercase: 
-	for (int j = 0; j < key.length(); j++)
-	{
-		if (isupper(key[j]))
-		{
-			key[j] = tolower(key[j]); 
-		}
-	}
-
-	std::string noSpacesPlain = "";
-
-	//Remove spaces from Plaintext: 
-	for (int k = 0; k < plainText.length(); k++)
-	{
-		if (isalpha(plainText[k]))
-		{
-			noSpacesPlain += plainText[k]; 
-		}
-	}
-
-	plainText = noSpacesPlain; 
- 
-
-	std::string noSpacesKey = "";
-
-	//Remove spaces from Key: 
-	for (int l = 0; l < key.length(); l++)
-	{
-		if (isalpha(key[l]))
-		{
-			noSpacesKey += key[l];
-		}
-	}
-
-	std::cout << noSpacesKey << std::endl; 
-	key = noSpacesKey; 
-
+	ConvertToUpper(plainText); 
+	ConvertToUpper(key); 
+	
+	RemoveSpaces(plainText); 
+	RemoveSpaces(key); 
+	
 	PadKey(plainText, key);
+
+	std::string encryption = "";
+	int codedLetter = 0; 
+
+	for (int i = 0; i < plainText.length(); i++)
+	{ 
+		encryption += ((plainText[i] + key[i]) % 26) + 'A';
+	}
+
+	std::cout << encryption << std::endl; 
 
 	return; 
 }
 
-void Encrypt::PadKey(std::string plaintext, std::string& key)
+
+void Encrypt::PadKey(const std::string plaintext, std::string& key)
 {
-	
-	int toPad = plaintext.length() - key.length(); 
-	int wholeWords = toPad / key.length(); 
-
-	std::string paddedKey = key; 
-
-	for (int i = 0; i < wholeWords; i++)
+	//If the key and the plaintext are the same length, do nothing: 
+	if (key.length() == plaintext.length())
 	{
-		paddedKey += key; 
+		return; 
 	}
 
+	//If the key is shorter than the plaintext, shorten the key: 
+	else if (key.length() > plaintext.length())
+	{
+		key = key.substr(0, plaintext.length() - 1); 
+		return; 
+	}
 
-		for (int j = 0; j < (toPad % key.length()); j++)
+	else
+	{
+		//Calculate the number of spaces that need to be padded: 
+		int toPad = plaintext.length() - key.length();
+
+		std::string paddedKey = key;
+
+		//Pad the key by the number of whole words available: 
+		for (int i = 0; i < (toPad / key.length()); i++)
 		{
-			paddedKey += key[j]; 
+			paddedKey += key;
 		}
 
-		key = paddedKey; 
-		std::cout << plaintext << std::endl; 
-		std::cout << key << std::endl; 
+		//Pad the key with however many remaining letters there are: 
+		for (int j = 0; j < (toPad % key.length()); j++)
+		{
+			paddedKey += key[j];
+		}
+
+		key = paddedKey;
+	}
+
+}
+
+void Encrypt::ConvertToUpper(std::string& text)
+{
+	//Convert String to all lowercase in preparation for coding:  
+	for (int i = 0; i < text.length(); i++)
+	{
+		if (islower(text[i]))
+		{
+			text[i] = toupper(text[i]);
+		}
+	}
+}
+
+void Encrypt::RemoveSpaces(std::string & text)
+{
+	std::string noSpaces = "";
+
+	//Remove spaces from Plaintext: 
+	for (int k = 0; k < text.length(); k++)
+	{
+		if (isalpha(text[k]))
+		{
+			noSpaces += text[k];
+		}
+	}
+
+	text = noSpaces;
+}
+
+void Encrypt::DoDecryption(std::string cipherText, std::string key)
+{
+	ConvertToUpper(cipherText); 
+	ConvertToUpper(key); 
+
+	RemoveSpaces(cipherText); 
+	RemoveSpaces(key);
+
+	PadKey(cipherText, key); 
+
+	std::string decryption = ""; 
+
+	for (int i = 0; i < cipherText.length(); i++)
+	{
+		decryption += ((cipherText[i] - key[i] + 26) % 26) + 'A';
+	}
+
+	std::cout << decryption << std::endl;
+
+	return;
 }
--- Vigenere/Vigenere/Encrypt.h
+++ Vigenere/Vigenere/Encrypt.h
@@ -14,9 +14,12 @@ class Encrypt
 public:
 	Encrypt();
 	~Encrypt();
-	static void DoEncryption(std::string plainText, std::string key); 
+	static void DoEncryption(std::string plainText, std::string key);
+	static void DoDecryption(std::string cipherText, std::string key); 
 
 private: 
-	static void PadKey(std::string plaintext, std::string& key); 
+	static void PadKey(const std::string plaintext, std::string& key);
+	static void ConvertToUpper(std::string& text); 
+	static void RemoveSpaces(std::string& text); 
 };
 
